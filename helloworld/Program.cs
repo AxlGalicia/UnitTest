a95137f8838cfa1b774f59f09516f477e378f417
AxlGalicia <axlgreatga@gmail.com>
2020-07-23 15:23:14 -0600

    Funcionalidad basica de un unit test

---
+++ helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace helloworld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int test(int valor){
+            if(valor>0){
+                return 1;
+            }else{
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ mstest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using helloworld;
+namespace mstest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Se inicializa la prueba con el parametro esperado en la respuesta del metodo test()
+            var resultado = 1;
+
+            //Se instancia un objeto de la clase Program
+            Program o = new Program();
+            //Se realiza la prueba del metodo
+            var result = o.test(2);
+            //Se comprueba los resultados de la prueba
+            Assert.AreEqual(resultado,result);
+        }
+    }
+}
